9fa2e66d8a8b12934aab8813633df80c171e5148
riched <rickard.hedlund@lm.se>
2025-10-13 23:38:40 +0200

    switch vesktop to use new method from RFE [#4](https://github.com/grvn/void-packages/issues/4)

--- srcpkgs/vesktop/update.sh
+++ srcpkgs/vesktop/update.sh
@@ -11,11 +11,8 @@ CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
-# No preprepped checksum files, need to download the binary and calculate it myself
-gh release download -R Vencord/Vesktop --archive=tar.gz --output "vesktop.tar.gz"
-export SHA256=$(sha256sum ./vesktop.tar.gz | cut -d ' ' -f1 )
-rm ./vesktop.tar.gz
-[[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
+export SHA256=$(gh release view ${VERSION} -R Vencord/Vesktop --json assets --jq ".assets[] | select(.name==\"vesktop-${LATEST_VERSION}.tar.gz\") | .digest" | cut -d":" -f2)
+[[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of checksum\n" && exit 1
 
 envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
 
