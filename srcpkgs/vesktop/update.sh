89037d029dc9d448dbb702bd4fc23bc42773c530
riched <rickard.hedlund@lm.se>
2026-03-29 00:23:38 +0100

    DRY

--- srcpkgs/vesktop/update.sh
+++ srcpkgs/vesktop/update.sh
@@ -4,14 +4,16 @@ printf "Checking latest version\n"
 
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
-LATEST_VERSION=$(gh release list --repo Vencord/Vesktop --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+REPO="Vencord/Vesktop"
+
+LATEST_VERSION=$(gh release list --repo ${REPO} --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
 export VERSION=${LATEST_VERSION#"v"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
-export SHA256=$(gh release view ${LATEST_VERSION} -R Vencord/Vesktop --json assets --jq ".assets[] | select(.name==\"vesktop-${VERSION}.tar.gz\") | .digest" | cut -d":" -f2)
+export SHA256=$(gh release view ${LATEST_VERSION} -R ${REPO} --json assets --jq ".assets[] | select(.name==\"vesktop-${VERSION}.tar.gz\") | .digest" | cut -d":" -f2)
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of checksum\n" && exit 1
 
 envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
--- srcpkgs/wideriver/update.sh
+++ srcpkgs/wideriver/update.sh
@@ -4,7 +4,9 @@ printf "Checking latest version\n"
 
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
-LATEST_VERSION=$(gh release list --repo alex-courtis/wideriver --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+REPO="alex-courtis/wideriver"
+
+LATEST_VERSION=$(gh release list --repo ${REPO} --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
 export VERSION=${LATEST_VERSION#"v"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
@@ -12,7 +14,7 @@ printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CU
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
 # No preprepped checksum files, need to download the binary and calculate it myself
-gh release download -R alex-courtis/wideriver --archive=tar.gz --output "wideriver.tar.gz"
+gh release download -R ${REPO} --archive=tar.gz --output "wideriver.tar.gz"
 export SHA256=$(sha256sum ./wideriver.tar.gz | cut -d ' ' -f1 )
 rm ./wideriver.tar.gz
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
